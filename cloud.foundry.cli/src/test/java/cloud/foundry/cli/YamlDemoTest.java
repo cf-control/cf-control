547500c3bff21495cd68baafb0f1da15bb66bf20
chris <chrizz.m@gmx.de>
2020-05-05 18:49:37 +0200

    - Nested JavaBeans Tests für die YAMLDemo hinzugefügt

--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/YamlDemoTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/YamlDemoTest.java
@@ -6,7 +6,9 @@ import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.representer.Representer;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.LinkedList;
 import java.util.HashMap;
@@ -20,7 +22,7 @@ public class YamlDemoTest {
                 "- three\n";
 
         Yaml yaml = new Yaml();
-        List<String> list = yaml.loadAs(yamlDocument, List.class);
+        List<Object> list = yaml.loadAs(yamlDocument, List.class);
 
         assertThat(list.size(), is(3));
         assertThat(list.get(0), is("one"));
@@ -49,6 +51,7 @@ public class YamlDemoTest {
 
         private String name;
         private int age;
+        private List<BankAccount> bankAccounts;
 
         // to fulfill the JavaBeans specification a class needs:
         // - a public constructor with no arguments
@@ -70,6 +73,38 @@ public class YamlDemoTest {
 
         public void setAge(int age) {
             this.age = age;
+        }
+
+        public List<BankAccount> getBankAccounts() {
+            return bankAccounts;
+        }
+
+        public void setBankAccounts(List<BankAccount> bankAccounts) {
+            this.bankAccounts = bankAccounts;
+        }
+    }
+
+    public static class BankAccount {
+
+        private String accountNumber;
+        private String bankCode;
+
+        public BankAccount(){}
+
+        public String getAccountNumber() {
+            return accountNumber;
+        }
+
+        public void setAccountNumber(String accountNumber) {
+            this.accountNumber = accountNumber;
+        }
+
+        public String getBankCode() {
+            return bankCode;
+        }
+
+        public void setBankCode(String bankCode) {
+            this.bankCode = bankCode;
         }
     }
 
@@ -98,6 +133,63 @@ public class YamlDemoTest {
         String yamlDocument = yaml.dumpAsMap(person);
 
         assertThat(yamlDocument, is("age: 21\n" +
-                "name: Jane Doe\n")); // note that the keys are sorted alphabetically by default
+                                        "bankAccounts: null\n" +
+                                        "name: Jane Doe\n"));
+        // note that the keys are sorted alphabetically by default
+    }
+
+    @Test
+    public void testLoadNestedSequenceAsJavaBean() {
+        String yamlDocument =   "age: 42\n" +
+                                "bankAccounts:\n" +
+                                "- accountNumber: '0009131958'\n" +
+                                "  bankCode: '57210000'\n" +
+                                "- accountNumber: '0009171113'\n" +
+                                "  bankCode: '57210000'\n" +
+                                "name: John Doe\n";
+
+        Yaml yaml = new Yaml();
+        Person person = yaml.loadAs(yamlDocument, Person.class);
+
+        assertThat(person.getName(), is("John Doe"));
+        assertThat(person.getAge(), is(42));
+        assertThat(person.getBankAccounts().size(), is(2));
+        assertThat(person.getBankAccounts().get(0).getAccountNumber(), is("0009131958"));
+        assertThat(person.getBankAccounts().get(0).getBankCode(), is("57210000"));
+        assertThat(person.getBankAccounts().get(1).getAccountNumber(), is("0009171113"));
+        assertThat(person.getBankAccounts().get(1).getBankCode(), is("57210000"));
+    }
+
+    @Test
+    public void testDumpNestedJavaBean() {
+        Person person = new Person();
+        person.setName("Jane Doe");
+        person.setAge(21);
+
+        BankAccount bankAccount1 = new BankAccount();
+        bankAccount1.setAccountNumber("0009131958");
+        bankAccount1.setBankCode("57210000");
+
+        BankAccount bankAccount2 = new BankAccount();
+        bankAccount2.setAccountNumber("0009171113");
+        bankAccount2.setBankCode("57210000");
+
+        person.setBankAccounts(Arrays.asList(bankAccount1, bankAccount2));
+
+        // do not dump tags into the document
+        DumperOptions options = new DumperOptions();
+        options.setTags(new HashMap<>());
+
+        Yaml yaml = new Yaml(options);
+        String yamlDocument = yaml.dumpAsMap(person);
+
+        // note that the keys are sorted alphabetically by default
+        assertThat(yamlDocument, is("age: 21\n" +
+                                        "bankAccounts:\n" +
+                                        "- accountNumber: '0009131958'\n" +
+                                        "  bankCode: '57210000'\n" +
+                                        "- accountNumber: '0009171113'\n" +
+                                        "  bankCode: '57210000'\n" +
+                                        "name: Jane Doe\n"));
     }
 }
