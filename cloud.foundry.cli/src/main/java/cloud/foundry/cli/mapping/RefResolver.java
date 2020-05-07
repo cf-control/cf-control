82bfdeecaae2f68269c73d98b61d71d9d3475f93
Felix Quast <felix.quast98@gmail.com>
2020-05-07 17:53:02 +0200

    Add class with interface for resolving $ref occurences (driver: Felix, navigators: Yen, Chris)

---
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/mapping/RefResolver.java
@@ -0,0 +1,13 @@
+package cloud.foundry.cli.mapping;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class RefResolver {
+
+    public static Object resolve(Object yamlTreeRoot, Yaml yamlParser) {
+        // TODO custom exceptions in case of error:
+        // e.g. unknown node type
+        return null;
+    }
+}
+
