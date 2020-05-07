76aaceabf37a50ef7bfeb24c4e9c0c2b9b505353
Felix Quast <felix.quast98@gmail.com>
2020-05-07 17:53:54 +0200

    Add class with interface for convenience functions for yaml trees (driver: Felix, navigators: Yen, Chris)

---
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/mapping/YamlTreeUtils.java
@@ -0,0 +1,12 @@
+package cloud.foundry.cli.mapping;
+
+public class YamlTreeUtils {
+
+    private static final char DELIMITER = '/';
+
+    public static Object getDescendantNode(Object yamlTree, String yamlPath) {
+        // TODO custom exceptions in case of error:
+        // e.g. node in path does not exist
+        return null;
+    }
+}
