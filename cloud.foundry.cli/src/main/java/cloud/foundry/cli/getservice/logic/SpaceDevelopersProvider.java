dd3d24806407f1e2865c2c8a8abdf7c4c585de77
yenle2501 <leyen.coburg@gmail.com>
2020-05-12 23:54:52 +0200

    testclass refactored

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/getservice/logic/SpaceDevelopersProvider.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/getservice/logic/SpaceDevelopersProvider.java
@@ -13,20 +13,23 @@ import org.yaml.snakeyaml.Yaml;
 import reactor.core.publisher.Mono;
 
 public class SpaceDevelopersProvider {
-
     private DefaultCloudFoundryOperations cfOperations;
 
     public SpaceDevelopersProvider(DefaultCloudFoundryOperations cfOperations) {
         this.cfOperations = cfOperations;
     }
 
+    /**
+     * List all space developers
+     * 
+     * @return list of space developers in YAML format
+     */
     public String getSpaceDevelopers() {
         ListSpaceUsersRequest request = ListSpaceUsersRequest.builder()
             .spaceName(cfOperations.getSpace()).organizationName(cfOperations.getOrganization())
             .build();
         Mono<SpaceUsers> spaceUsers = cfOperations.userAdmin().listSpaceUsers(request);
         SpaceUsers users = spaceUsers.block();
-        
         List<String> listDevelopers = users.getDevelopers();
         Map<String, List<String>> data = new HashMap<String, List<String>>();
         data.put("spaceDevelopers", listDevelopers);
