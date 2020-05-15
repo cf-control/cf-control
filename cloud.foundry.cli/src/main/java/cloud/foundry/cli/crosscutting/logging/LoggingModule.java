6682c53714e7c5f04487df989ee15c7e069f3d4a
Felix Quast <felix.quast98@gmail.com>
2020-05-15 20:00:16 +0200

    Add docstring for dummy class files

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/logging/LoggingModule.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/logging/LoggingModule.java
@@ -1,4 +1,9 @@
 package cloud.foundry.cli.crosscutting.logging;
 
+/**
+ * This class serves as a temporary dummy file. It makes sure that the otherwise empty package-folder is considered by
+ * source control.
+ * It has no functionality at the moment and might be renamed or deleted in the future.
+ */
 public class LoggingModule {
 }
