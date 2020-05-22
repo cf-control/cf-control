0fd1890eaffee70e5073c6b86c1e1072ff35bf50
TobiCode <tobias.w.weber@gmail.com>
2020-05-22 10:35:59 +0200

    Documentation for CfOperationsCreator.

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/CfOperationsCreator.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/CfOperationsCreator.java
@@ -10,9 +10,21 @@ import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
 
 public class CfOperationsCreator {
 
+    /**
+     * Names of the environment variables that hold the username and the password value for the application.
+     */
     private static final String CF_CONTROL_USER = "CF_CONTROL_USER";
     private static final String CF_CONTROL_PASSWORD = "CF_CONTROL_PASSWORD";
 
+    /**
+     * Creates the cfOperations object, which is needed by our operations classes in the package. If the username and
+     * password is not set within the commandOptions, then it tries to get the values from the System Environment
+     * Variables: "CF_CONTROL_USER" & "CF_CONTROL_PASSWORD"
+     * {@link cloud.foundry.cli.operations}. The cfOpeartions object
+     * @param commandOptions {@link LoginCommandOptions}
+     * @return A DefaultCloudFoundryOperations object, which is the entry point for accessing
+     * the CF configurations.
+     */
     public static DefaultCloudFoundryOperations createCfOperations(
             LoginCommandOptions commandOptions) {
 
