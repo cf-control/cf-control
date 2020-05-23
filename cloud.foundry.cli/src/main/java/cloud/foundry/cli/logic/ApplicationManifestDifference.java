a8123cb7b0813c006ab90374ed07bb61bbc36647
Felix Quast <felix.quast98@gmail.com>
2020-05-23 10:33:50 +0200

    Add initial implementation of the application differ

---
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/ApplicationManifestDifference.java
@@ -0,0 +1,16 @@
+package cloud.foundry.cli.logic;
+
+import cloud.foundry.cli.crosscutting.beans.ApplicationManifestBean;
+
+/**
+ * TODO doc
+ */
+public class ApplicationManifestDifference extends AbstractDifference<ApplicationManifestBean> {
+
+    /**
+     * TODO doc
+     */
+    public ApplicationManifestDifference(ApplicationManifestBean affected) {
+        super(affected);
+    }
+}
