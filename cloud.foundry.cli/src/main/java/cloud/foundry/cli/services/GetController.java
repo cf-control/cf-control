eb798936b4529433eb1a492476a602d5ef02d020
TobiCode <tobias.w.weber@gmail.com>
2020-05-23 12:33:46 +0200

    Change the header and the description of the Commands.

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/GetController.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/GetController.java
@@ -1,8 +1,8 @@
 package cloud.foundry.cli.services;
 
 import cloud.foundry.cli.crosscutting.beans.ApplicationBean;
-import cloud.foundry.cli.crosscutting.beans.ServiceBean;
 import cloud.foundry.cli.crosscutting.beans.GetAllBean;
+import cloud.foundry.cli.crosscutting.beans.ServiceBean;
 import cloud.foundry.cli.crosscutting.beans.SpaceDevelopersBean;
 import cloud.foundry.cli.crosscutting.mapping.CfOperationsCreator;
 import cloud.foundry.cli.crosscutting.util.YamlCreator;
@@ -10,29 +10,20 @@ import cloud.foundry.cli.operations.AllInformationOperations;
 import cloud.foundry.cli.operations.ApplicationOperations;
 import cloud.foundry.cli.operations.ServicesOperations;
 import cloud.foundry.cli.operations.SpaceDevelopersOperations;
-import org.cloudfoundry.client.CloudFoundryClient;
-import org.cloudfoundry.client.v2.info.GetInfoRequest;
-import org.cloudfoundry.client.v2.info.GetInfoResponse;
-import org.cloudfoundry.client.v2.info.Info;
 import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
-import org.cloudfoundry.reactor.DefaultConnectionContext;
-import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Mixin;
 
-import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class realizes the functionality that is needed for the get commands. They provide various information about a
  * cloud foundry instance.
  */
 @Command(name = "get",
-        header = "%n@|green Get-Controller|@",
+        header = "%n@|green Get the current configuration of your cf instance.|@",
+        mixinStandardHelpOptions = true,
         subcommands = {
                 GetController.GetServicesCommand.class,
                 GetController.GetSpaceDevelopersCommand.class,
@@ -43,10 +34,14 @@ public class GetController implements Runnable {
     @Override
     public void run() {
         // this code is executed if the user runs the get command without specifying any sub-command
+        CommandLine.usage(this, System.out);
+        return;
     }
 
     @Command(name = "space-developers",
-            description = "List all space developers in the target space")
+            description = "List all space developers in the target space.",
+            mixinStandardHelpOptions = true
+    )
     static class GetSpaceDevelopersCommand implements Runnable {
         @Mixin
         LoginCommandOptions loginOptions;
@@ -61,7 +56,7 @@ public class GetController implements Runnable {
         }
     }
 
-    @Command(name = "services", description = "List all services in the target space")
+    @Command(name = "services", description = "List all services in the target space.")
     static class GetServicesCommand implements Runnable {
         @Mixin
         LoginCommandOptions loginOptions;
@@ -76,7 +71,7 @@ public class GetController implements Runnable {
         }
     }
 
-    @Command(name = "applications", description = "List all applications in the target space")
+    @Command(name = "applications", description = "List all applications in the target space.")
     static class GetApplicationsCommand implements Runnable {
         @Mixin
         LoginCommandOptions loginOptions;
@@ -91,7 +86,7 @@ public class GetController implements Runnable {
         }
     }
 
-    @Command(name = "all", description = "show all information in the target space")
+    @Command(name = "all", description = "Show all information in the target space.")
     static class GetAllInformation implements Runnable {
         @Mixin
         LoginCommandOptions loginOptions;
