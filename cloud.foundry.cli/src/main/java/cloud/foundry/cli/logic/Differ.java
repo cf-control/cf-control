68c2716df356340c9d956bc813571340e04acfa4
Felix Quast <felix.quast98@gmail.com>
2020-05-24 11:46:37 +0200

    Enhance diff implementation

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
@@ -14,21 +14,47 @@ import java.util.Arrays;
 import java.util.LinkedList;
 
 import cloud.foundry.cli.crosscutting.beans.ApplicationBean;
+import cloud.foundry.cli.crosscutting.beans.Bean;
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.diff.Change;
+import org.javers.core.diff.Diff;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+
 
 /**
  * TODO doc
  */
 public class Differ {
 
+    private static final Javers JAVERS = JaversBuilder.javers().build();
+
     /**
      * TODO doc
      */
-    public static Set<ApplicationDifference> diffApplications(Set<ApplicationBean> presentApplications,
-                                                              Set<ApplicationBean> desiredApplications) {
-        DifferenceCreator differenceCreator = new DifferenceCreator();
-        return differenceCreator.createApplicationDifference(presentApplications, desiredApplications);
+    public static Set<ApplicationDiff> diffApplications(Set<ApplicationBean> presentApplications,
+                                                        Set<ApplicationBean> desiredApplications) {
+        Diff diff = JAVERS.compareCollections(presentApplications, desiredApplications, ApplicationBean.class);
+
+        List<Change> changes = diff.getChanges();
+        ApplicationDiffVisitor visitor = new ApplicationDiffVisitor();
+        for (Change change : changes) {
+            Optional<Object> possiblyAffectedByChange = change.getAffectedObject();
+
+            // this should always be guaranteed by the diff
+            assert possiblyAffectedByChange.isPresent();
+
+            Object affectedByChange = possiblyAffectedByChange.get();
+            if (affectedByChange instanceof Bean) {
+                Bean affectedBean = (Bean) affectedByChange;
+                visitor.setCurrentChange(change);
+                affectedBean.visit(visitor);
+            }
+        }
+        return visitor.getApplicationDiffs();
     }
     private static final Javers JAVERS = JaversBuilder.javers().build();
     private static final String ROOT_SYMBOL = "#";
