a75062f23f990e2be97b87df6a08fc2dca65a03b
chris <chrizz.m@gmx.de>
2020-05-27 11:48:33 +0200

    set up the foundation for the new approach for handling the diff issue

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
@@ -1,15 +1,17 @@
 package cloud.foundry.cli.logic;
 
-import cloud.foundry.cli.crosscutting.beans.ApplicationBean;
-import cloud.foundry.cli.crosscutting.beans.Bean;
+import cloud.foundry.cli.crosscutting.beans.ConfigBean;
+import cloud.foundry.cli.crosscutting.beans.SpecBean;
 import org.javers.core.Javers;
 import org.javers.core.JaversBuilder;
 import org.javers.core.diff.Change;
 import org.javers.core.diff.Diff;
+import org.javers.core.diff.ListCompareAlgorithm;
+import org.javers.core.diff.changetype.NewObject;
+import org.javers.core.diff.changetype.ObjectRemoved;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 
 /**
@@ -18,29 +20,55 @@ import java.util.Set;
 public class Differ {
 
     private static final Javers JAVERS = JaversBuilder.javers().build();
+    private static final String ROOT_SYMBOL = "#";
+    private static final String PATH_SEPARATOR_SYMBOL = "/";
+    private static final String ROOT_NAME = "root";
 
     /**
      * TODO doc
      */
-    public static Set<ApplicationDiff> diffApplications(Set<ApplicationBean> presentApplications,
-                                                        Set<ApplicationBean> desiredApplications) {
-        Diff diff = JAVERS.compareCollections(presentApplications, desiredApplications, ApplicationBean.class);
+    public static DiffNode createDiffTree(ConfigBean liveConfig, ConfigBean desiredConfig) {
+        /**
+         * Javers stores changes in a Change class, which holds information about the absolute path
+         * from the Root Object (ConfigBean here) to the actual level where a change has taken place.
+         * The idea is to build a tree data structure where each node holds the changes of their level.
+         */
 
-        List<Change> changes = diff.getChanges();
-        ApplicationDiffVisitor visitor = new ApplicationDiffVisitor();
-        for (Change change : changes) {
-            Optional<Object> possiblyAffectedByChange = change.getAffectedObject();
+        Diff diff = JAVERS.compare(liveConfig, desiredConfig);
 
-            // this should always be guaranteed by the diff
-            assert possiblyAffectedByChange.isPresent();
+        DiffNode diffNode = DiffNode.create(ROOT_NAME);
+        for (Change change: diff.getChanges()) {
+           if (change instanceof ObjectRemoved) continue;
 
-            Object affectedByChange = possiblyAffectedByChange.get();
-            if (affectedByChange instanceof Bean) {
-                Bean affectedBean = (Bean) affectedByChange;
-                visitor.setCurrentChange(change);
-                affectedBean.visit(visitor);
-            }
+            LinkedList<String> path = extractPath(change);
+            replaceRoot(ROOT_NAME, path);
+            diffNode.insert(path, change);
+
         }
-        return visitor.getApplicationDiffs();
+
+        return diffNode;
     }
+
+    /**
+     * for example:
+     * change.getAffectedGlobalId() = cloud.foundry.cli.crosscutting.bean.ConfigBean/#spec/apps/someApp/manifest
+     *                           -> [cloud.foundry.cli.crosscutting.bean.ConfigBean, spec, apps, someApp, manifest]
+     */
+    private static LinkedList<String> extractPath(Change change) {
+        return new LinkedList<>(Arrays.asList(change
+                .getAffectedGlobalId()
+                .toString()
+                .replace(ROOT_SYMBOL, "")
+                .split(PATH_SEPARATOR_SYMBOL)));
+    }
+
+    /**
+     * example: from [cloud.foundry.cli.crosscutting.bean.ConfigBean, spec, apps, someApp, manifest]
+     *          to [{propertyName}, spec, apps, someApp, manifest]
+     */
+    private static void replaceRoot(String propertyName, LinkedList<String> path) {
+        path.removeFirst();
+        path.addFirst(propertyName);
+    }
+
 }
