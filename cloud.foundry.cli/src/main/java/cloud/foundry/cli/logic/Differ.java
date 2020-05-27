f3c5c0cbfb6c9c569b9eb0ad51bc6d67867de946
chris <chrizz.m@gmx.de>
2020-05-27 11:48:33 +0200

    set up the foundation for the new approach for handling the diff issue

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/Differ.java
@@ -13,49 +13,12 @@ import org.javers.core.diff.changetype.ObjectRemoved;
 import java.util.Arrays;
 import java.util.LinkedList;
 
-import cloud.foundry.cli.crosscutting.beans.ApplicationBean;
-import cloud.foundry.cli.crosscutting.beans.Bean;
-import org.javers.core.Javers;
-import org.javers.core.JaversBuilder;
-import org.javers.core.diff.Change;
-import org.javers.core.diff.Diff;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
 
 /**
  * TODO doc
  */
 public class Differ {
 
-    private static final Javers JAVERS = JaversBuilder.javers().build();
-
-    /**
-     * TODO doc
-     */
-    public static Set<ApplicationDiff> diffApplications(Set<ApplicationBean> presentApplications,
-                                                        Set<ApplicationBean> desiredApplications) {
-        Diff diff = JAVERS.compareCollections(presentApplications, desiredApplications, ApplicationBean.class);
-
-        List<Change> changes = diff.getChanges();
-        ApplicationDiffVisitor visitor = new ApplicationDiffVisitor();
-        for (Change change : changes) {
-            Optional<Object> possiblyAffectedByChange = change.getAffectedObject();
-
-            // this should always be guaranteed by the diff
-            assert possiblyAffectedByChange.isPresent();
-
-            Object affectedByChange = possiblyAffectedByChange.get();
-            if (affectedByChange instanceof Bean) {
-                Bean affectedBean = (Bean) affectedByChange;
-                visitor.setCurrentChange(change);
-                affectedBean.visit(visitor);
-            }
-        }
-        return visitor.getApplicationDiffs();
-    }
     private static final Javers JAVERS = JaversBuilder.javers().build();
     private static final String ROOT_SYMBOL = "#";
     private static final String PATH_SEPARATOR_SYMBOL = "/";
