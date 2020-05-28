54ce1c407d21fbdd58da4d5c16c6d31262b1be69
chris <chrizz.m@gmx.de>
2020-05-28 18:50:01 +0200

    add empty constructor as well as getter and setter to conform to Bean specification

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/beans/ConfigBean.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/beans/ConfigBean.java
@@ -5,4 +5,32 @@ public class ConfigBean {
     String apiVersion;
     TargetBean target;
     SpecBean spec;
+
+    public ConfigBean() {
+
+    }
+
+    public String getApiVersion() {
+        return apiVersion;
+    }
+
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    public TargetBean getTarget() {
+        return target;
+    }
+
+    public void setTarget(TargetBean target) {
+        this.target = target;
+    }
+
+    public SpecBean getSpec() {
+        return spec;
+    }
+
+    public void setSpec(SpecBean spec) {
+        this.spec = spec;
+    }
 }
