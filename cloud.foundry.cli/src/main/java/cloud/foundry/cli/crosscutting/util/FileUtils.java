cbaa8f5695a86aa6c8168d41e1564be686775a8d
Felix Quast <felix.quast98@gmail.com>
2020-05-28 09:48:09 +0200

    Adapt changes of master branch

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/util/FileUtils.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/util/FileUtils.java
@@ -51,7 +51,7 @@ public class FileUtils {
 
     private static String doReadLocalFile(String filePath) throws IOException {
         File file = new File(filePath);
-        if (!isYamlFile(file.getName())) {
+        if (hasUnallowedFileExtension(file.getName())) {
             throw new InvalidFileTypeException("invalid file extension: "
                     + FilenameUtils.getExtension(file.getName()));
         } else if (emptyFileExtension(file.getName())) {
@@ -60,8 +60,52 @@ public class FileUtils {
         return readFile(file);
     }
 
-    private static boolean isYamlFile(String filename) {
-        return ALLOWED_FILE_EXTENSIONS.contains(FilenameUtils.getExtension(filename).toUpperCase());
+    /**
+     *
+     * @param url   url to the host e.g. https://host.com/path/to/file.yml
+     * @return      the content of the file as a String
+     * @throws IOException
+     */
+    public static String readRemoteFile(String url) throws IOException, ProtocolException {
+        checkNotNull(url);
+
+        String content = doReadRemoteFile(url);
+
+        assert content != null;
+        return content;
+    }
+
+    private static String doReadRemoteFile(String url) throws IOException, ProtocolException {
+        URI uri = URI.create(url);
+
+        if (hasUnallowedFileExtension(uri.getPath()) && !emptyFileExtension(uri.getPath())) {
+            throw new cloud.foundry.cli.exceptions.InvalidFileTypeException("invalid file extension: "
+                    + FilenameUtils.getExtension(uri.getPath()));
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+
+            try (CloseableHttpResponse response = httpClient.execute(new HttpGet(uri))) {
+
+                //TODO: more precise exception handling
+                if (response.getCode() != 200) {
+                    throw new HttpResponseException(response.getCode(), response.getReasonPhrase());
+                }
+
+                if (response.getHeader("Content-Type") != null
+                        && !response.getHeader("Content-Type").equals("text/plain")) {
+
+                    throw new HttpResponseException(response.getCode(),
+                            "invalid content type, was: " + response.getHeader("Content-Type"));
+                }
+
+                return EntityUtils.toString(response.getEntity());
+            }
+        }
+    }
+
+    private static boolean hasUnallowedFileExtension(String filename) {
+        return !ALLOWED_FILE_EXTENSIONS.contains(FilenameUtils.getExtension(filename).toUpperCase());
     }
 
     public static boolean emptyFileExtension(String filename) {
