ecb498f29b680064af8111f6e2f92c33845f6547
chris <chrizz.m@gmx.de>
2020-06-05 12:12:27 +0200

    Add documentation

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/output/DiffStringBuilder.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/output/DiffStringBuilder.java
@@ -7,6 +7,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 //TODO rename to DiffLineBuilder
+
+/**
+ * Builder class that let's the user build a diff line with given properties
+ *  <ul>
+ *      <li>indentation: spaces left to the actual string</li>
+ *      <li>propertyName: name of the property (a colon will be added after the property name)</li>
+ *      <li>value: value that comes after the property name</li>
+ *      <li>flagSymbol: the symbol that is the first character of the diff line ('+', '-', ...)</li>
+ *      <li>wrapperColor: color the console should print the line in</li>
+ *  </ul>
+ *
+ * example:
+ * <ul>
+ *      <li>flagSymbol = FlagSymbol.Added</li>
+ *      <li>indentation = 6</li>
+ *      <li> propertyName = 'size'</li>
+ *      <li>value = '1024'</li>
+ *      <li>withNewLine = true</li>
+ *      <br>
+ *      <li> Result: "[color]+ &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp size: 1024[color.reset]"</li>
+ *  </ul>
+ */
 public class DiffStringBuilder {
 
     private static final Map<FlagSymbol, WrapperColor> colorMapping;
@@ -32,33 +54,66 @@ public class DiffStringBuilder {
         value = "";
     }
 
+    /**
+     *
+     * @return the builder object
+     */
     public static DiffStringBuilder builder() {
         return new DiffStringBuilder();
     }
 
+    /**
+     *
+     * @param indentation spaces left to the actual string
+     * @return instance of the current builder object
+     */
     public DiffStringBuilder setIndentation(int indentation) {
         this.indentation = indentation;
         return this;
     }
 
+    /**
+     *
+     * @param propertyName name of the property (a colon will be added after the property name)
+     * @return instance of the current builder object
+     * @throws NullPointerException
+     */
     public DiffStringBuilder setPropertyName(@Nonnull String propertyName) {
         checkNotNull(propertyName);
         this.propertyName = propertyName;
         return this;
     }
 
+    /**
+     *
+     * @param flagSymbol the symbol that is the first character of the diff line ('+', '-', ...)
+     * @return instance of the current builder object
+     * @throws NullPointerException
+     */
     public DiffStringBuilder setFlagSymbol(@Nonnull FlagSymbol flagSymbol) {
         checkNotNull(flagSymbol);
         this.flagSymbol = flagSymbol;
         return this;
     }
 
+    /**
+     *
+     * @param value value that comes after the property name
+     * @return instance of the current builder object
+     * @throws NullPointerException
+     */
     public DiffStringBuilder setValue(@Nonnull String value) {
         checkNotNull(value);
         this.value = value;
         return this;
     }
 
+    /**
+     *
+     * @param wrapperColor color the console should print the line in
+     * @return instance of the current builder object
+     * @throws NullPointerException
+     */
     public DiffStringBuilder setWrapperColor(@Nonnull WrapperColor wrapperColor) {
         checkNotNull(wrapperColor);
         this.wrapperColor = wrapperColor;
@@ -66,17 +121,8 @@ public class DiffStringBuilder {
     }
 
     /**
-     * TODO: needs better formatting here
-     * {color}{flagSymbol} ' 'x{indentation} + [propertyName: ] + [value] + {colorReset} + {withNewLine ? true => \n}
-     * for example:
-     *             flagSymbol = FlagSymbol.Added,
-     *             indentation = 6,
-     *             propertyName = 'size',
-     *             value = '1024',
-     *             withNewLine = true
-     *
-     *     Result: "+       size: 1024\n"
-     * @return a string as shown above
+     * build the diff line with the given parameters of the build process
+     * @return the diff line as string
      */
     public String build() {
         StringBuilder sb = new StringBuilder();
