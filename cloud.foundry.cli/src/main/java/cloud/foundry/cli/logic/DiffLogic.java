c9e866ebc8f51e0760e00571735761c471f04bcb
chris <chrizz.m@gmx.de>
2020-06-09 03:33:23 +0200

    Change parameter type to bean to allow any bean to be diffed and refactor

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/DiffLogic.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/DiffLogic.java
@@ -1,22 +1,59 @@
 package cloud.foundry.cli.logic;
 
-import cloud.foundry.cli.crosscutting.exceptions.UnsupportedChangeTypeException;
-import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
+import cloud.foundry.cli.crosscutting.exceptions.DiffException;
+import cloud.foundry.cli.crosscutting.mapping.beans.Bean;
 import cloud.foundry.cli.logic.diff.DiffNode;
 import cloud.foundry.cli.logic.diff.Differ;
 import cloud.foundry.cli.logic.diff.output.DiffOutput;
 
-import javax.annotation.Nonnull;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class DiffLogic {
 
-    public DiffNode createDiffTree(@Nonnull ConfigBean liveConfig,@Nonnull ConfigBean desiredConfig) {
-        return Differ.createDiffTree(liveConfig, desiredConfig);
+    /**
+     *
+     * @param liveConfig
+     * @param desiredConfig
+     * @return
+     * @throws DiffException
+     */
+    public DiffNode createDiffTree(Bean liveConfig, Bean desiredConfig) throws DiffException {
+        try{
+            checkNotNull(liveConfig);
+            checkNotNull(desiredConfig);
+
+            return doCreateDiffTree(liveConfig, desiredConfig);
+        } catch (Exception e) {
+            throw new DiffException(e.getMessage(), e);
+        }
     }
 
-    public String createDiffOutput(@Nonnull ConfigBean liveConfig,@Nonnull ConfigBean desiredConfig)
-            throws UnsupportedChangeTypeException {
-        DiffNode diffNode = Differ.createDiffTree(liveConfig, desiredConfig);
+    private DiffNode doCreateDiffTree(Bean liveConfig, Bean desiredConfig) {
+        Differ differ = new Differ();
+        return differ.createDiffTree(liveConfig, desiredConfig);
+    }
+
+    /**
+     *
+     * @param liveConfig
+     * @param desiredConfig
+     * @return
+     * @throws DiffException
+     */
+    public String createDiffOutput(Bean liveConfig, Bean desiredConfig) throws DiffException {
+        try{
+            checkNotNull(liveConfig);
+            checkNotNull(desiredConfig);
+
+            return doCreateDiffOutput(liveConfig, desiredConfig);
+        } catch (Exception e) {
+            throw new DiffException(e.getMessage(), e);
+        }
+    }
+
+    private String doCreateDiffOutput(Bean liveConfig, Bean desiredConfig) {
+        Differ differ = new Differ();
+        DiffNode diffNode = differ.createDiffTree(liveConfig, desiredConfig);
         DiffOutput diffOutput = new DiffOutput();
         return diffOutput.from(diffNode);
     }
