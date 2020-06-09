991a947408c400891cef1b6126d985bd08f4a165
chris <chrizz.m@gmx.de>
2020-06-09 19:05:00 +0200

    Add tests for the DiffLogic class

--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/DiffLogicTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/DiffLogicTest.java
@@ -1,25 +1,39 @@
 package cloud.foundry.cli.logic;
 
 import cloud.foundry.cli.crosscutting.exceptions.DiffException;
-import cloud.foundry.cli.crosscutting.mapping.YamlMapper;
 import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
+import cloud.foundry.cli.crosscutting.mapping.beans.SpecBean;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-// TODO: remove this temporary test class, when no longer needed
-
+/**
+ * Test for {@link DiffLogic}
+ */
 public class DiffLogicTest {
 
-    //TODO replace this dummy test with actual tests
     @Test
-    public void testDiffLogic() throws IOException, DiffException {
-        DiffLogic diffLogic = new DiffLogic();
+    public void testCreateDiffTreeOnNullThrowsException() {
+        assertThrows(DiffException.class, () -> new DiffLogic().createDiffTree(null, new ConfigBean()));
+        assertThrows(DiffException.class, () -> new DiffLogic().createDiffTree(new ConfigBean(), null));
+    }
 
-        ConfigBean configLive = YamlMapper.loadBean("src/test/resources/basic/configLive.yml", ConfigBean.class);
-        ConfigBean configDesired = YamlMapper.loadBean("src/test/resources/basic/configDesired.yml", ConfigBean.class);
+    @Test
+    public void testCreateDiffOutputOnNullThrowsException() {
+        assertThrows(DiffException.class, () -> new DiffLogic().createDiffOutput(null, new ConfigBean()));
+        assertThrows(DiffException.class, () -> new DiffLogic().createDiffOutput(new ConfigBean(), null));
+    }
 
-        System.out.println(diffLogic.createDiffOutput(configLive, configDesired));
+    @Test
+    public void testCreateDiffTreeOfDifferentBeanTypesThrowsException() {
+        assertThrows(DiffException.class,
+                () -> new DiffLogic().createDiffTree(new SpecBean(), new ConfigBean()));
+    }
+
+    @Test
+    public void testCreateDiffOutputOfDifferentBeanTypesThrowsException() {
+        assertThrows(DiffException.class,
+                () -> new DiffLogic().createDiffOutput(new SpecBean(), new ConfigBean()));
     }
 }
