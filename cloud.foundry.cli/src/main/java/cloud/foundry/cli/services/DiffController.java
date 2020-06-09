11d784d362f942c5c45ae6e9e335ea5ae2bef990
chris <chrizz.m@gmx.de>
2020-06-09 14:52:03 +0200

    Create diff controller

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/DiffController.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/DiffController.java
@@ -1,8 +1,71 @@
 package cloud.foundry.cli.services;
 
+import cloud.foundry.cli.crosscutting.mapping.CfOperationsCreator;
+import cloud.foundry.cli.crosscutting.mapping.beans.ApplicationBean;
+import cloud.foundry.cli.crosscutting.mapping.beans.SpecBean;
+import cloud.foundry.cli.crosscutting.util.FileUtils;
+import cloud.foundry.cli.crosscutting.util.YamlCreator;
+import cloud.foundry.cli.logic.DiffLogic;
+import cloud.foundry.cli.operations.ApplicationsOperations;
+import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
+import org.yaml.snakeyaml.Yaml;
+import picocli.CommandLine;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import static picocli.CommandLine.usage;
+
 /**
  * This class realizes the functionality that is needed for the diff commands. They provide a comparison of the
  * state of a cloud foundry instance with a provided configuration file.
  */
-public class DiffController {
+@CommandLine.Command(name = "diff",
+        header = "%n@|green Get the current configuration of your cf instance.|@",
+        mixinStandardHelpOptions = true,
+        subcommands = {
+                DiffController.DiffApplicationCommand.class})
+public class DiffController implements Callable<Integer> {
+
+    @Override
+    public Integer call() {
+        usage(this, System.out);
+        return 0;
+    }
+
+    @CommandLine.Command(name = "applications", description = "Show the differences between apps of the live " +
+            "system and apps specified in the yaml file.")
+    static class DiffApplicationCommand implements Callable<Integer> {
+
+        @CommandLine.Mixin
+        LoginCommandOptions loginOptions;
+
+        @CommandLine.Mixin
+        YamlCommandOptions commandOptions;
+
+        @Override
+        public Integer call() throws Exception{
+            String yamlFileContent = FileUtils.readLocalFile(commandOptions.getYamlFilePath());
+            Yaml yamlLoader = YamlCreator.createDefaultYamlProcessor();
+            DefaultCloudFoundryOperations cfOperations = CfOperationsCreator.createCfOperations(loginOptions);
+            ApplicationsOperations applicationsOperations = new ApplicationsOperations(cfOperations);
+
+            SpecBean specBeanDesired = yamlLoader.loadAs(yamlFileContent, SpecBean.class);
+            Map<String, ApplicationBean> appsLive = applicationsOperations.getAll().block();
+
+            SpecBean specBeanLive = new SpecBean();
+            specBeanLive.setApps(appsLive);
+
+            DiffLogic diffLogic = new DiffLogic();
+            String output = diffLogic.createDiffOutput(specBeanLive, specBeanDesired);
+
+            if(output.isEmpty()) {
+                System.out.println("There are no differences.");
+            }else {
+                System.out.println(output);
+            }
+
+            return 0;
+        }
+    }
 }
