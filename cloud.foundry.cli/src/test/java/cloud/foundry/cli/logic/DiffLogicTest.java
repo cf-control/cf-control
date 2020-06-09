47428c027910634ea03bff4edc72d55242ed9b0c
chris <chrizz.m@gmx.de>
2020-06-09 15:41:21 +0200

    Use YamlMapper in diff package

--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/DiffLogicTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/DiffLogicTest.java
@@ -1,14 +1,11 @@
 package cloud.foundry.cli.logic;
 
 import cloud.foundry.cli.crosscutting.exceptions.DiffException;
+import cloud.foundry.cli.crosscutting.mapping.YamlMapper;
 import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
-import cloud.foundry.cli.crosscutting.util.YamlCreator;
 import org.junit.jupiter.api.Test;
-import org.yaml.snakeyaml.Yaml;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 // TODO: remove this temporary test class, when no longer needed
@@ -17,14 +14,11 @@ public class DiffLogicTest {
 
     //TODO replace this dummy test with actual tests
     @Test
-    public void testDiffLogic() throws FileNotFoundException, DiffException {
-        Yaml yamlProc = YamlCreator.createDefaultYamlProcessor();
+    public void testDiffLogic() throws IOException, DiffException {
         DiffLogic diffLogic = new DiffLogic();
 
-        ConfigBean configLive = yamlProc.loadAs(new FileInputStream(
-            new File("src/test/resources/basic/configLive.yml")), ConfigBean.class);
-        ConfigBean configDesired = yamlProc.loadAs(new FileInputStream(
-            new File("src/test/resources/basic/configDesired.yml")), ConfigBean.class);
+        ConfigBean configLive = YamlMapper.loadBean("src/test/resources/basic/configLive.yml", ConfigBean.class);
+        ConfigBean configDesired = YamlMapper.loadBean("src/test/resources/basic/configDesired.yml", ConfigBean.class);
 
         System.out.println(diffLogic.createDiffOutput(configLive, configDesired));
     }
