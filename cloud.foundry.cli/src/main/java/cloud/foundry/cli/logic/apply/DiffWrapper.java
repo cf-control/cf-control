691cbdf973d1e8a4fa511a2d93f1ac9ef4a267c9
chris <chrizz.m@gmx.de>
2020-06-12 14:41:46 +0200

    Add implementation and tests for the diff wrapper

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/apply/DiffWrapper.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/apply/DiffWrapper.java
@@ -7,16 +7,18 @@ import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
 import cloud.foundry.cli.crosscutting.mapping.beans.SpecBean;
 import cloud.foundry.cli.logic.diff.DiffNode;
 import cloud.foundry.cli.logic.diff.change.CfChange;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerChange;
+import cloud.foundry.cli.logic.diff.change.object.CfObjectValueChanged;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.Field;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Collections;
-import java.util.HashSet;
 
 /**
  * This class is the result of a diff operation.
@@ -26,8 +28,12 @@ import java.util.HashSet;
 public class DiffWrapper {
 
     // lists the field names of bean classes that are needed in this class
+    private static final String TARGET_FIELD_NAME = "target";
     private static final String SPEC_FIELD_NAME = "spec";
     private static final String APPS_FIELD_NAME = "apps";
+    private static final String SERVICES_FIELD_NAME = "services";
+    private static final String SPACE_DEVELOPERS_PROPERTY_NAME = "spaceDevelopers";
+    private static final String API_VERSION_PROPERTY_NAME = "apiVersion";
 
     // ensure that the bean classes have fields with according names and types
     static {
@@ -36,14 +42,18 @@ public class DiffWrapper {
     }
 
     private final DiffNode rootNode;
+    private final DiffNode targetNode;
     private final DiffNode specNode;
     private final DiffNode appsNode;
+    private final DiffNode servicesNode;
 
     public DiffWrapper(@Nonnull DiffNode rootNode) {
         checkNotNull(rootNode);
         this.rootNode = rootNode;
+        this.targetNode = rootNode.getChild(TARGET_FIELD_NAME);
         this.specNode = rootNode.getChild(SPEC_FIELD_NAME);
         this.appsNode = specNode == null ? null : specNode.getChild(APPS_FIELD_NAME);
+        this.servicesNode = specNode == null ? null : specNode.getChild(SERVICES_FIELD_NAME);
     }
 
     private static Field checkFieldExists(Class<?> classWithField, String fieldName, Class<?> fieldType) {
@@ -69,5 +79,81 @@ public class DiffWrapper {
         assert genericTypes[0].equals(keyType);
         assert genericTypes[1].equals(valueType);
     }
+
+    /**
+     * @return a map containing the name of the app as key and the corresponding list of changes as entry
+     */
+    public Map<String, List<CfChange>> getApplicationChanges() {
+        return getAllChangesFromMapNode(appsNode);
+    }
+
+    /**
+     * @return a map containing the name of the service as key and the corresponding list of changes as entry
+     */
+    public Map<String, List<CfChange>> getServiceChanges() {
+        return getAllChangesFromMapNode(servicesNode);
+    }
+
+    private Map<String, List<CfChange>> getAllChangesFromMapNode(DiffNode node) {
+        Map<String, List<CfChange>> serviceChanges = new HashMap<>();
+
+        if (node != null) {
+            for (DiffNode childNode : node.getChildNodes()) {
+                serviceChanges.put(childNode.getPropertyName(), getAllChanges(childNode));
+            }
+        }
+
+        return serviceChanges;
+    }
+
+    /**
+     * @return a list of changes that occurred on the target bean
+     */
+    public List<CfChange> getTargetChanges() {
+        if (targetNode != null) {
+            return getAllChanges(targetNode);
+        }
+        return Collections.emptyList();
+    }
+
+    private List<CfChange> getAllChanges(DiffNode node) {
+        List<CfChange> result = new LinkedList<>();
+        doGetAllChanges(node, result);
+        return result;
+    }
+
+    private void doGetAllChanges(DiffNode node, List<CfChange> list) {
+        list.addAll(node.getChanges());
+        for (DiffNode child : node.getChildNodes()) {
+            doGetAllChanges(child, list);
+        }
+    }
+
+    /**
+     * @return the change object if there was a change else null
+     */
+    public CfContainerChange getSpaceDevelopersChange() {
+        return (CfContainerChange) getChange(specNode, SPACE_DEVELOPERS_PROPERTY_NAME);
+    }
+
+    /**
+     * @return the change object if there was a change else null
+     */
+    public CfObjectValueChanged getApiVersionChange() {
+        return (CfObjectValueChanged) getChange(rootNode, API_VERSION_PROPERTY_NAME);
+    }
+
+    private CfChange getChange(DiffNode node, String propertyName) {
+        if (node != null) {
+            return node.getChanges()
+                    .stream()
+                    .filter(change -> propertyName.equals(change.getPropertyName()))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return null;
+    }
+
+
 }
 
---
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/apply/DiffWrapperTest.java
@@ -0,0 +1,250 @@
+package cloud.foundry.cli.logic.apply;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+import cloud.foundry.cli.logic.diff.DiffNode;
+import cloud.foundry.cli.logic.diff.change.CfChange;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerChange;
+import cloud.foundry.cli.logic.diff.change.map.CfMapChange;
+import cloud.foundry.cli.logic.diff.change.object.CfNewObject;
+import cloud.foundry.cli.logic.diff.change.object.CfObjectValueChanged;
+import cloud.foundry.cli.logic.diff.change.object.CfRemovedObject;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+import java.util.Map;
+
+public class DiffWrapperTest {
+
+    @Test
+    public void testConstructorOnNullNodeThrowsException() {
+        // when and then
+        assertThrows(NullPointerException.class, () -> new DiffWrapper(null));
+    }
+
+
+    @Test
+    public void testGetApplicationChangesWithDataReturnsCorrectMapOfChanges() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(createTreeStructure());
+
+        // when
+        Map<String, List<CfChange>> applicationChanges = diffWrapper.getApplicationChanges();
+
+        // then
+        assertThat(applicationChanges, notNullValue());
+        assertThat(applicationChanges.size(), Matchers.is(2));
+        assertThat(applicationChanges, hasKey("newApp"));
+        assertThat(applicationChanges.get("newApp"), notNullValue());
+        assertThat(applicationChanges.get("newApp").size(), is(1));
+        assertThat(applicationChanges.get("newApp"), contains(instanceOf(CfNewObject.class)));
+        assertThat(applicationChanges, hasKey("someApp"));
+        assertThat(applicationChanges.get("someApp"), notNullValue());
+        assertThat(applicationChanges.get("someApp").size(), is(2));
+        assertThat(applicationChanges.get("someApp"),hasItems(
+                instanceOf(CfObjectValueChanged.class),
+                instanceOf(CfMapChange.class)));
+
+    }
+
+    @Test
+    public void testGetApplicationChangesWithEmptyDataReturnsEmptyMap() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(new DiffNode("config"));
+
+        // when
+        Map<String, List<CfChange>> applicationChanges = diffWrapper.getApplicationChanges();
+
+        // then
+        assertThat(applicationChanges, notNullValue());
+        assertThat(applicationChanges.size(), is(0));
+    }
+
+    @Test
+    public void testGetServicesChangesReturnsCorrectMapOfChanges() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(createTreeStructure());
+
+        // when
+        Map<String, List<CfChange>> serviceChanges = diffWrapper.getServiceChanges();
+
+        // then
+        assertThat(serviceChanges, notNullValue());
+        assertThat(serviceChanges.size(), Matchers.is(2));
+        assertThat(serviceChanges, hasKey("removedService"));
+        assertThat(serviceChanges.get("removedService"), notNullValue());
+        assertThat(serviceChanges.get("removedService").size(), is(1));
+        assertThat(serviceChanges.get("removedService"), contains(instanceOf(CfRemovedObject.class)));
+        assertThat(serviceChanges, hasKey("someService"));
+        assertThat(serviceChanges.get("someService"), notNullValue());
+        assertThat(serviceChanges.get("someService").size(), is(1));
+        assertThat(serviceChanges.get("someService"),hasItems(
+                instanceOf(CfContainerChange.class)));
+    }
+
+    @Test
+    public void testGetServiceChangesWithEmptyDataReturnsEmptyMap() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(new DiffNode("config"));
+
+        // when
+        Map<String, List<CfChange>> serviceChanges = diffWrapper.getServiceChanges();
+
+        // then
+        assertThat(serviceChanges, notNullValue());
+        assertThat(serviceChanges.size(), is(0));
+    }
+
+    @Test
+    public void testGetApiVersionChangeWithDataReturnsCorrectChange() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(createTreeStructure());
+
+        // when
+        CfChange apiVersionChange = diffWrapper.getApiVersionChange();
+
+        // then
+        assertThat(apiVersionChange, notNullValue());
+        assertThat(apiVersionChange, instanceOf(CfObjectValueChanged.class));
+        assertThat(apiVersionChange.getPropertyName(), is("apiVersion"));
+    }
+
+    @Test
+    public void testGetApiVersionChangeWithEmptyDataReturnsNull() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(new DiffNode("config"));
+
+        // when
+        CfChange apiVersionChange = diffWrapper.getApiVersionChange();
+
+        // then
+        assertThat(apiVersionChange, nullValue());
+    }
+
+    @Test
+    public void testGetSpaceDevelopersChangeWithDataReturnsCorrectChange() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(createTreeStructure());
+
+        // when
+        CfChange spaceDevelopersChange = diffWrapper.getSpaceDevelopersChange();
+
+        // then
+        assertThat(spaceDevelopersChange, notNullValue());
+        assertThat(spaceDevelopersChange, instanceOf(CfContainerChange.class));
+        assertThat(spaceDevelopersChange.getPropertyName(), is("spaceDevelopers"));
+    }
+
+    @Test
+    public void testGetSpaceDevelopersChangeWithEmptyDataReturnsNull() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(new DiffNode("config"));
+
+        // when
+        CfChange spaceDevelopersChange = diffWrapper.getSpaceDevelopersChange();
+
+        // then
+        assertThat(spaceDevelopersChange, nullValue());
+    }
+
+    @Test
+    public void testGetTargetChangesWithDataReturnsListOfChanges() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(createTreeStructure());
+
+        // when
+        List<CfChange> targetChanges = diffWrapper.getTargetChanges();
+
+        // then
+        assertThat(targetChanges, notNullValue());
+        assertThat(targetChanges.size(), Matchers.is(1));
+        assertThat(targetChanges, hasItems(instanceOf(CfObjectValueChanged.class)));
+        assertThat(targetChanges.get(0).getPropertyName(), is("space"));
+    }
+
+    @Test
+    public void testGetTargetChangesWithNoDataReturnsEmptyList() {
+        // given
+        DiffWrapper diffWrapper = new DiffWrapper(new DiffNode("config"));
+
+        // when
+        List<CfChange> targetChanges = diffWrapper.getTargetChanges();
+
+        // then
+        assertThat(targetChanges, notNullValue());
+        assertThat(targetChanges.size(), is(0));
+    }
+
+    private DiffNode createTreeStructure() {
+        DiffNode configNode = new DiffNode("config");
+        DiffNode targetNode = new DiffNode("target");
+        DiffNode specNode = new DiffNode("spec");
+        DiffNode appsNode = new DiffNode("apps");
+        DiffNode servicesNode = new DiffNode("services");
+        DiffNode newAppNode = new DiffNode("newApp");
+        DiffNode removedServiceNode = new DiffNode("removedService");
+        DiffNode someAppNode = new DiffNode("someApp");
+        DiffNode someAppManifestNode = new DiffNode("manifest");
+        DiffNode someAppEnvVarNode = new DiffNode("environmentVariables");
+        DiffNode someServiceNode = new DiffNode("someService");
+        DiffNode someServiceTagsNode = new DiffNode("tags");
+
+        configNode.addChild(targetNode);
+        configNode.addChild(specNode);
+        specNode.addChild(appsNode);
+        specNode.addChild(servicesNode);
+        appsNode.addChild(newAppNode);
+        appsNode.addChild(someAppNode);
+        servicesNode.addChild(removedServiceNode);
+        servicesNode.addChild(someServiceNode);
+        someAppNode.addChild(someAppManifestNode);
+        someAppManifestNode.addChild(someAppEnvVarNode);
+        someServiceNode.addChild(someServiceTagsNode);
+
+        CfObjectValueChanged apiVersionChange = Mockito.mock(CfObjectValueChanged.class);
+        when(apiVersionChange.getPropertyName()).thenReturn("apiVersion");
+
+        CfObjectValueChanged spaceChange = Mockito.mock(CfObjectValueChanged.class);
+        when(spaceChange.getPropertyName()).thenReturn("space");
+
+        CfContainerChange spaceDevelopersChange = Mockito.mock(CfContainerChange.class);
+        when(spaceDevelopersChange.getPropertyName()).thenReturn("spaceDevelopers");
+
+        CfNewObject newAppChange = Mockito.mock(CfNewObject.class);
+        when(newAppChange.getPropertyName()).thenReturn("newApp");
+
+        CfRemovedObject removedServiceChange = Mockito.mock(CfRemovedObject.class);
+        when(newAppChange.getPropertyName()).thenReturn("removedService");
+
+        CfMapChange mapChange = Mockito.mock(CfMapChange.class);
+        when(newAppChange.getPropertyName()).thenReturn("environmentVariables");
+
+        CfObjectValueChanged pathChange = Mockito.mock(CfObjectValueChanged.class);
+        when(newAppChange.getPropertyName()).thenReturn("path");
+
+        CfContainerChange containerChange = Mockito.mock(CfContainerChange.class);
+        when(newAppChange.getPropertyName()).thenReturn("tags");
+
+        configNode.addChange(apiVersionChange);
+        targetNode.addChange(spaceChange);
+        specNode.addChange(spaceDevelopersChange);
+        newAppNode.addChange(newAppChange);
+        removedServiceNode.addChange(removedServiceChange);
+        someAppNode.addChange(pathChange);
+        someAppManifestNode.addChange(mapChange);
+        someServiceNode.addChange(containerChange);
+
+        return configNode;
+    }
+}
