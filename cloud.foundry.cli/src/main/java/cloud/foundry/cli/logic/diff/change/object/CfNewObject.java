f5177dfb258f619edf6672128e9491a6a8e74363
TobiCode <tobias.w.weber@gmail.com>
2020-06-16 15:21:24 +0200

    Changes according to review (Docu updated)

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/change/object/CfNewObject.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/change/object/CfNewObject.java
@@ -22,7 +22,7 @@ public class CfNewObject extends CfChange {
     }
 
     /**
-     * Accept a visitor handling that specific type of change object. Used for the applyLogic
+     * Accept a visitor handling that specific type of change object.
      * @param visitor the concrete visitor to work on that object.
      */
     @Override
