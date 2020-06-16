1292337499054f12eba81456b7b990051ea60fb4
F <m4eqp@noreply.users.github.com>
2020-06-16 22:10:20 +0200

    Configure logging verbosity with CLI options
    Introduces -v/-d/-q options in the CLI.

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/BaseController.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/BaseController.java
@@ -31,6 +31,27 @@ import java.util.concurrent.Callable;
         UpdateController.class})
 public class BaseController implements Callable<Integer> {
 
+    @CommandLine.Option(names = {"-q", "--quiet"}, description = "Reduce log verbosity and print errors only.")
+    private boolean quiet;
+
+    @CommandLine.Option(names = {"-v", "--verbose"}, description = "Enable verbose logging.")
+    private boolean verbose;
+
+    @CommandLine.Option(names = {"-d", "--debug"}, description = "Enable debug logging.")
+    private boolean debug;
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public boolean isQuiet() {
+        return quiet;
+    }
+
     @Override
     public Integer call() {
         // this code is executed if the user just runs the app
@@ -39,7 +60,15 @@ public class BaseController implements Callable<Integer> {
     }
 
     public static void main(String[] args) {
-        CommandLine cli = new CommandLine(new BaseController());
+        // now, this is a little annoying, but it seems picocli doesn't provide any other option
+        // in order to be able to handle the global logging options, we need to access the values in the base
+        // controller
+        // unfortunately, these are populated only once we call parseArgs or execute
+        // therefore, we have to parse the arguments twice: once with a parseArgs() call to be able to handle the
+        // logging options, then again in the execute() call which runs the subcommands etc.
+        BaseController controller = new BaseController();
+
+        CommandLine cli = new CommandLine(controller);
 
         // picocli has a nice hidden feature: one can register a special exception handler and thus deal with
         // exceptions occurring during the execution of a Callable, Runnable etc.
@@ -93,10 +122,32 @@ public class BaseController implements Callable<Integer> {
             // no need for returning different exit codes for different exceptions, but that's also possible in the
             // future
             return 1;
-        }
-        );
+        });
 
+        // parse args now to be able to configure logging before we continue running the rest of the CLI
+        cli.parseArgs(args);
+
+        // now, we can access the logging options in the base controller
+        // note: we always enable the most verbose level the user specifies
+        // for that reason we can't use an if-else, but must use a chain of plain if clauses
+        // TODO: check if mutually exclusive argument group provides a better UX
+        if (controller.isQuiet()) {
+            // wouldn't make sense to log that we enabled the quiet mode, right?
+            // the whole idea is to reduce the amount of log messages
+            Log.setQuietLogLevel();
+        }
+        if (controller.isVerbose()) {
+            Log.setVerboseLogLevel();
+            Log.verbose("enabling verbose logging");
+        }
+        if (controller.isDebug()) {
+            Log.setDebugLogLevel();
+            Log.debug("enabling debug logging");
+        }
+
+        // okay, logging is configured, now let's run the rest of the CLI
         int exitCode = cli.execute(args);
+
         System.exit(exitCode);
     }
 
