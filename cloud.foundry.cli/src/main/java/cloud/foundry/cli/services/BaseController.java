f1dde84f911a38a07a63d2fdac8830c4c62867b1
Felix Quast <felix.quast98@gmail.com>
2020-06-16 16:03:47 +0200

    Add ApplyException to the exception handling

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/BaseController.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/services/BaseController.java
@@ -4,6 +4,7 @@ import cloud.foundry.cli.crosscutting.exceptions.CreationException;
 import cloud.foundry.cli.crosscutting.exceptions.CredentialException;
 import cloud.foundry.cli.crosscutting.exceptions.DiffException;
 import cloud.foundry.cli.crosscutting.exceptions.RefResolvingException;
+import cloud.foundry.cli.crosscutting.exceptions.ApplyException;
 import cloud.foundry.cli.crosscutting.logging.Log;
 import cloud.foundry.cli.crosscutting.mapping.RefResolver;
 import org.yaml.snakeyaml.constructor.ConstructorException;
@@ -62,6 +63,8 @@ public class BaseController implements Callable<Integer> {
                 Log.error("Cannot interpret yaml contents:", ex.getMessage());
             } else if (ex instanceof DiffException) {
                 Log.error("Unable to perform the diff:", ex.getMessage());
+            } else if (ex instanceof ApplyException) {
+                Log.error("An error occurred during the apply:", ex.getMessage());
             } else if (ex instanceof IllegalStateException) {
                 // a little bit ugly, but it works
                 // the problem is in reactor.core.Exceptions
