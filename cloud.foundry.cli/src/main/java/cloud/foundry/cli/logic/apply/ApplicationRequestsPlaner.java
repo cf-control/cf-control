128217d2607661b0599bb9ac0cf45d922f449ceb
chris <chrizz.m@gmx.de>
2020-06-23 17:26:33 +0200

    Adjust doc

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/apply/ApplicationRequestsPlaner.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/apply/ApplicationRequestsPlaner.java
@@ -2,7 +2,6 @@ package cloud.foundry.cli.logic.apply;
 
 import cloud.foundry.cli.crosscutting.exceptions.ApplyException;
 import cloud.foundry.cli.crosscutting.exceptions.CreationException;
-import cloud.foundry.cli.crosscutting.logging.Log;
 import cloud.foundry.cli.crosscutting.mapping.beans.ApplicationBean;
 import cloud.foundry.cli.crosscutting.mapping.beans.ApplicationManifestBean;
 import cloud.foundry.cli.logic.diff.change.CfChange;
@@ -19,23 +18,23 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * This class is responsible to apply in the context of applications according to the CfChanges.
- * The class performs the apply task by implementing the {@link CfChangeVisitor} interface.
+ * This class is responsible to build the requests in the context of applications according to the CfChanges.
+ * The class does creates the request tasks by implementing the {@link CfChangeVisitor} interface.
  */
-public class ApplyApplicationsPlaner implements CfChangeVisitor {
+public class ApplicationRequestsPlaner implements CfChangeVisitor {
 
     private final ApplicationsOperations appOperations;
     private final String applicationName;
     private final List<Mono<Void>> requests;
 
-    private ApplyApplicationsPlaner(ApplicationsOperations appOperations, String applicationName) {
+    private ApplicationRequestsPlaner(ApplicationsOperations appOperations, String applicationName) {
         this.appOperations = appOperations;
         this.applicationName = applicationName;
         this.requests = new LinkedList<>();
     }
 
     /**
-     * Apply logic for CfNewObject
+     * Creates the request for CfNewObject
      * @param newObject the CfNewObject to be visited
      * @throws IllegalArgumentException if the newObject is neither an ApplicationBean or an ApplicationManifestBean
      * @throws ApplyException that wraps the exceptions that can occur during the creation procedure.
@@ -61,7 +60,7 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
     }
 
     /**
-     * Apply logic for CfObjectValueChanged
+     * Creates the requests for CfObjectValueChanged
      * @param objectValueChanged the CfObjectValueChanged to be visited
      */
     @Override
@@ -71,7 +70,7 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
     }
 
     /**
-     * Apply logic for CfRemovedObject
+     * Creates the requests for CfRemovedObject
      * @param removedObject the CfRemovedObject to be visited
      */
     @Override
@@ -81,7 +80,7 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
     }
 
     /**
-     * Apply logic for CfContainerChange
+     * Creates the requests CfContainerChange
      * @param containerChange the CfContainerChange to be visited
      */
     @Override
@@ -91,7 +90,7 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
     }
 
     /**
-     * Apply logic for CfMapChange
+     * Creates the requests for CfMapChange
      * @param mapChange the CfMapChange to be visited
      */
     @Override
@@ -101,7 +100,7 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
     }
 
     /**
-     * Apply for all changes regarding one application. If an error occurs, the applying procedure is
+     * Creates the requests for one application. If an error occurs, the applying procedure is
      * discontinued and the app stops.
      * @param appOperations the ApplicationOperations object used for
      * @param applicationName the name of the application
@@ -109,14 +108,16 @@ public class ApplyApplicationsPlaner implements CfChangeVisitor {
      * @throws ApplyException if an error during the apply logic occurs. May contain another exception inside
      * with more details
      * @throws IllegalArgumentException if the newObject is neither an ApplicationBean or an ApplicationManifestBean
+     * @return Flux of all requests that are required to apply the changes
      */
     public static Flux<Void> create(ApplicationsOperations appOperations, String applicationName,
                                     List<CfChange> applicationChanges) {
-        ApplyApplicationsPlaner applyApplicationsPlaner = new ApplyApplicationsPlaner(appOperations, applicationName);
+        ApplicationRequestsPlaner applicationRequestsPlaner = new ApplicationRequestsPlaner(appOperations,
+                applicationName);
         for (CfChange applicationChange : applicationChanges) {
-            applicationChange.accept(applyApplicationsPlaner);
+            applicationChange.accept(applicationRequestsPlaner);
         }
 
-        return Flux.merge(applyApplicationsPlaner.requests);
+        return Flux.merge(applicationRequestsPlaner.requests);
     }
 }
