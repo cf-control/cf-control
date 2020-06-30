878eee5d9b47cd22ec0339b048c13b4578e72fd5
TobiCode <tobias.w.weber@gmail.com>
2020-06-30 23:56:12 +0200

    Add tests for creating new services

--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/apply/ServicesRequestsPlanerTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/apply/ServicesRequestsPlanerTest.java
@@ -8,9 +8,11 @@ import static org.mockito.Mockito.when;
 
 import cloud.foundry.cli.crosscutting.exceptions.ApplyException;
 import cloud.foundry.cli.crosscutting.exceptions.UpdateException;
+import cloud.foundry.cli.crosscutting.mapping.beans.ApplicationBean;
 import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
 import cloud.foundry.cli.crosscutting.mapping.beans.ServiceBean;
 import cloud.foundry.cli.logic.diff.change.CfChange;
+import cloud.foundry.cli.logic.diff.change.object.CfNewObject;
 import cloud.foundry.cli.logic.diff.change.object.CfRemovedObject;
 import cloud.foundry.cli.operations.ServicesOperations;
 import org.junit.jupiter.api.Test;
@@ -20,9 +22,60 @@ import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class ServicesRequestsPlanerTest {
+
+    @Test
+    public void testCreateWithNewObjectSucceeds() {
+        // given
+        ServiceBean newServiceMock = mock(ServiceBean.class);
+        ServicesOperations servicesOperations = mock(ServicesOperations.class);
+        CfChange newChange = new CfNewObject(newServiceMock, "someservice", Collections.singletonList("root"));
+        Mono<Void> monoMock = mock(Mono.class);
+        Mockito.when(servicesOperations.create("someservice", newServiceMock)).thenReturn(monoMock);
+        List<CfChange> cfChanges = new LinkedList<>();
+        cfChanges.add(newChange);
+        // when
+        Flux<Void> requests = ServiceRequestsPlaner.create(servicesOperations,
+                "someservice",
+                cfChanges);
+
+        //then
+        assertThat(requests, notNullValue());
+    }
+
+    @Test
+    public void testCreateWithNewObjectNullpointerException() {
+        // given
+        ServiceBean newServiceBean = new ServiceBean();
+        ServicesOperations servicesOperations = mock(ServicesOperations.class);
+        CfChange newChange = new CfNewObject(newServiceBean, "someservice",
+                Collections.singletonList("root"));
+        Mockito.when(servicesOperations.create("someservice", newServiceBean))
+                .thenThrow(new NullPointerException());
+        List<CfChange> cfChanges = new LinkedList<>();
+        cfChanges.add(newChange);
+        // when
+        assertThrows(ApplyException.class,
+                () -> ServiceRequestsPlaner.create(servicesOperations, "someservice", cfChanges));
+    }
+
+    @Test
+    public void testCreateWithNewObjectNotServiceBean() {
+        // given
+        ApplicationBean newApplicationBean = new ApplicationBean();
+        ServicesOperations servicesOperations = mock(ServicesOperations.class);
+        CfChange newChange = new CfNewObject(newApplicationBean, "someservice",
+                Collections.singletonList("root"));
+        List<CfChange> cfChanges = new LinkedList<>();
+        cfChanges.add(newChange);
+        // when
+        assertThrows(IllegalArgumentException.class,
+                () -> ServiceRequestsPlaner.create(servicesOperations, "someservice", cfChanges));
+    }
 
     @Test
     public void testCreateWithRemovedObjectThrowsExceptionWhenThereAreAlreadyOtherRequests() {
