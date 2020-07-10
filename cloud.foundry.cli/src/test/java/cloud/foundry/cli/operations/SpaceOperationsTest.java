8d3f705b0beb2da567061e6d7d02d9e2c93e9223
Felix Quast <felix.quast98@gmail.com>
2020-07-10 22:15:44 +0200

    Fix test case for succeeding get all

--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/operations/SpaceOperationsTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/operations/SpaceOperationsTest.java
@@ -7,9 +7,11 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -17,24 +19,34 @@ import static org.mockito.Mockito.when;
 public class SpaceOperationsTest {
 
     @Test
-    public void testGetAllSucceeds(){
+    public void testGetAllSucceeds() {
         //given
+        final String firstSpaceName = "someSpaceName";
+        final String secondSpaceName = "someOtherSpaceName";
+
+        List<SpaceSummary> spaceSummaries = Arrays.asList(
+                SpaceSummary.builder()
+                        .name(firstSpaceName)
+                        .id("someId").build(),
+                SpaceSummary.builder()
+                        .name(secondSpaceName)
+                        .id("someOtherId").build()
+        );
+        Flux<SpaceSummary> spaceSummariesFlux = Flux.fromIterable(spaceSummaries);
+
         DefaultCloudFoundryOperations cloudFoundryOperationsMock = mock(DefaultCloudFoundryOperations.class);
         Spaces spacesMock = mock(Spaces.class);
-        Flux<SpaceSummary> spaceSummaryFluxMock = mock(Flux.class);
-        Flux<String> spaceNameFluxMock = mock(Flux.class);
-        Mono<List<String>> spaceNamesMono = mock(Mono.class);
 
         when(cloudFoundryOperationsMock.spaces()).thenReturn(spacesMock);
-        when(spacesMock.list()).thenReturn(spaceSummaryFluxMock);
-        when(spaceSummaryFluxMock.map(spaceSummary -> spaceSummary.getName())).thenReturn(spaceNameFluxMock);
-        when(spaceNameFluxMock.collectList()).thenReturn(spaceNamesMono);
+        when(spacesMock.list()).thenReturn(spaceSummariesFlux);
+
+        SpaceOperations spaceOperations = new SpaceOperations(cloudFoundryOperationsMock);
 
         //when
-        SpaceOperations spaceOperations = new SpaceOperations(cloudFoundryOperationsMock);
         Mono<List<String>> result = spaceOperations.getAll();
 
         //then
-        assertThat(result, is(spaceNamesMono));
+        List<String> resultingSpaceNames = result.block();
+        assertThat(resultingSpaceNames, containsInAnyOrder(firstSpaceName, secondSpaceName));
     }
 }
