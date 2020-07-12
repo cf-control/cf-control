c9654bb3bf32b998b7bc12c81c0f78925f891ce3
yenle2501 <leyen.coburg@gmail.com>
2020-07-12 15:41:46 +0200

    implemented feature get default params from yaml file

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/CfArgumentsCreator.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/CfArgumentsCreator.java
@@ -13,10 +13,13 @@ import java.util.List;
 import java.util.Properties;
 
 import cloud.foundry.cli.crosscutting.logging.Log;
+import cloud.foundry.cli.crosscutting.mapping.beans.ConfigBean;
+import cloud.foundry.cli.crosscutting.mapping.beans.TargetBean;
 
 /**
- * This utility class creates certain options with default values, which were not passed when the command was called.
- * There are currently default values for <code>api</code>, <code>organization</code> and <code>space</code>.
+ * This utility class creates certain options with default values, which were
+ * not passed when the command was called. There are currently default values
+ * for <code>api</code>, <code>organization</code> and <code>space</code>.
  */
 public class CfArgumentsCreator {
 
@@ -28,14 +31,21 @@ public class CfArgumentsCreator {
     private static final String CF_CONTROL_PROPERTIES = "cf-control.properties";
 
     /**
-     * This method reads in arguments passed on the command line.
-     * The tool uses default values unless the user overwrites them by passing parameters on the CLI.
-     * The default values are defined in a properties file which is compiled into the application.
-     *
-     * Example:
-     * A call "command -s <space name> -a <API endpoint>" would be extended to
-     * "command -s <space name> -a <API endpoint> -o <default organization name>",
+     * This method reads in arguments passed on the command line. The tool uses
+     * default values unless the user overwrites them by passing parameters on the
+     * CLI. For <code>get</code> command, the default values are defined in a
+     * properties file. Otherwise for the <code>diff</code>/<code>apply</code> command,
+     * the default values can be fetched from the given YAML file.
+     * 
+     * Example: A call "get -s <space name> -a <API endpoint>" would be extended to
+     * "get -s <space name> -a <API endpoint> -o <default organization name>",
      * where the default organization name is taken from the properties file.
+     * 
+     * A call "diff -s <space name> -a <API endpoint> -y <Path to YAML file>" would
+     * be extended to "diff -s <space name> -a <API endpoint> -y <Path to YAML File> -o <default organization name>",
+     * where the default organization name is taken
+     * from the given YAML file.
+     * 
      *
      * @param cli  CommandLine interpreter
      * @param args Commandline arguments
@@ -43,12 +53,13 @@ public class CfArgumentsCreator {
      * @throws picocli.CommandLine.ParameterException if the arguments are invalid
      */
     public static String[] determineCommandLine(CommandLine cli, String[] args, CommandLine.ParseResult subcommand) {
-        // exclude commands which don't use the cloud.foundry.cli.services.LoginCommandOptions mixin
-        // TODO: find out whether it's possible to recognize the LoginCommandOptions mixin with reflections only
+        // exclude commands which don't use the
+        // cloud.foundry.cli.services.LoginCommandOptions mixin
+        // TODO: find out whether it's possible to recognize the LoginCommandOptions
+        // mixin with reflections only
         if (subcommand != null && subcommand.commandSpec().userObject() instanceof DumpController) {
             return args;
         }
-
 
         List<String> optionNames = Arrays.asList("-a", "-o", "-s");
         List<String> missingOptions = new ArrayList<>();
@@ -67,17 +78,27 @@ public class CfArgumentsCreator {
 
         log.verbose("User has not passed values for arguments ", missingOptions, ", using default values");
 
-        return extendCommandLine(missingOptions, new LinkedList<>(Arrays.asList(args)));
+        // get missing values from config-properties File and extend to get command
+        if (Arrays.asList(args).contains("get")) {
+
+            return extendForGetCommand(missingOptions, new LinkedList<>(Arrays.asList(args)));
+        }
+        // get missing values from the given YAML File and extend to diff/apply command
+        else {
+
+            return extendForDiffAndApplyCommand(missingOptions, new LinkedList<>(Arrays.asList(args)));
+        }
     }
 
     /**
-     * Extends the commandLine with the missing options by reading the associated default value from a property file.
+     * Extends the commandLine with the missing options by reading the associated
+     * default value from a property file.
      *
      * @param missingOptions missing Options like -s, -a, -o
      * @param args           Commandline arguments
      * @return Commandline arguments
      */
-    private static String[] extendCommandLine(List<String> missingOptions, LinkedList<String> args) {
+    private static String[] extendForGetCommand(List<String> missingOptions, LinkedList<String> args) {
         try {
             ClassLoader classLoader = CfArgumentsCreator.class.getClassLoader();
             InputStream input = classLoader.getResourceAsStream(CF_CONTROL_PROPERTIES);
@@ -90,7 +111,49 @@ public class CfArgumentsCreator {
                 args.add(prop.getProperty(key));
 
                 log.info("Extended CommandLine Argument with the Option: " + key +
-                        " and value " + "'" + prop.getProperty(key) + "'");
+                    " and value " + "'" + prop.getProperty(key) + "'");
+            });
+        } catch (IOException ex) {
+            log.error(ex.getMessage());
+            System.exit(1);
+        }
+
+        return args.toArray(new String[0]);
+    }
+
+    /**
+     * Extends the commandLine with the missing options by reading the given yaml file.
+     *
+     * @param missingOptions missing Options like -s, -a, -o
+     * @param args           Commandline arguments
+     * @return Commandline arguments
+     */
+    private static String[] extendForDiffAndApplyCommand(List<String> missingOptions, LinkedList<String> args) {
+        try {
+            String yamlPath = args.get(args.indexOf("-y") + 1);
+            ConfigBean configBean = YamlMapper.loadBeanFromFile(yamlPath, ConfigBean.class);
+            TargetBean targetBean = configBean.getTarget();
+
+            missingOptions.forEach(key -> {
+                args.add(key);
+                String value = "";
+                switch (key) {
+                case "-a":
+                    value = targetBean.getEndpoint();
+                    break;
+                case "-o":
+                    value = targetBean.getOrg();
+                    break;
+                case "-s":
+                    value = targetBean.getSpace();
+                    break;
+                default:
+                    break;
+                }
+                
+                args.add(value);
+                log.info("Extended CommandLine Argument with the Option: " + key +
+                    " and value " + "'" + value + "'");
             });
         } catch (IOException ex) {
             log.error(ex.getMessage());
