2ea071cc9f45a6fd5c9a01fd0a4a2672b56dde9f
chris <chrizz.m@gmx.de>
2020-07-15 10:24:47 +0200

    Add doc to ObjectPropertyValidation

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/validation/ObjectPropertyValidation.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/validation/ObjectPropertyValidation.java
@@ -6,8 +6,18 @@ import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This class provides utility methods for static field property assertion.
+ */
 public class ObjectPropertyValidation {
 
+    /**
+     * Tests if the class has the given field with the given field type
+     * @param classWithField class to check the field
+     * @param fieldName name of the field
+     * @param fieldType type of the field
+     * @return reflection object of the field
+     */
     public static Field checkFieldExists(Class<?> classWithField, String fieldName, Class<?> fieldType) {
         Field field;
         try {
@@ -21,6 +31,12 @@ public class ObjectPropertyValidation {
         return field;
     }
 
+    /**
+     * Tests if the class has the given list field with the given list type
+     * @param classWithField class to check the list field
+     * @param fieldName name of the list field
+     * @param elementType type of the list field
+     */
     public static void checkListExists(Class<?> classWithField, String fieldName, Class<?> elementType) {
         Field field = checkFieldExists(classWithField, fieldName, List.class);
         ParameterizedType genericType = (ParameterizedType) field.getGenericType();
@@ -30,6 +46,13 @@ public class ObjectPropertyValidation {
         assert genericTypes[0].equals(elementType);
     }
 
+    /**
+     * Tests if the class has the given list field with the given list type
+     * @param classWithField class to check the map field
+     * @param fieldName name of the map field
+     * @param keyType type of the key of the map field
+     * @param valueType type of the value of the map field
+     */
     public static void checkMapExists(Class<?> classWithField, String fieldName,
                                        Class<?> keyType, Class<?> valueType) {
         Field field = checkFieldExists(classWithField, fieldName, Map.class);
