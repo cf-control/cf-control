32c70432e568eb3d142684dc2633dbce84c70d11
chris <chrizz.m@gmx.de>
2020-07-21 12:27:33 +0200

    Fix edge case when space gets created and JaVers finds no space developers change explicitly

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/change/parsing/NewObjectParsingStrategy.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/logic/diff/change/parsing/NewObjectParsingStrategy.java
@@ -1,14 +1,20 @@
 package cloud.foundry.cli.logic.diff.change.parsing;
 
 import cloud.foundry.cli.crosscutting.logging.Log;
+import cloud.foundry.cli.crosscutting.mapping.beans.SpecBean;
 import cloud.foundry.cli.logic.diff.change.CfChange;
+import cloud.foundry.cli.logic.diff.change.ChangeType;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerChange;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerValueChanged;
 import cloud.foundry.cli.logic.diff.change.object.CfNewObject;
 import org.javers.core.diff.Change;
 import org.javers.core.diff.changetype.NewObject;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * This class parses change objects of type {@link NewObject} to a single
@@ -25,9 +31,32 @@ public class NewObjectParsingStrategy extends AbstractParsingStrategy {
 
     @Override
     protected List<CfChange> doParse(Change change) {
+        List<CfChange> cfChanges = new LinkedList<>();
+
+        // since JaVers doesn't explicitly create a change object for the space developers when spec bean is a new object
+        // so creating it manually
+        if(change.getAffectedObject().get() instanceof SpecBean) {
+            List<String> spaceDevelopers = ((SpecBean) change.getAffectedObject().get()).getSpaceDevelopers();
+            if (spaceDevelopers != null) {
+                String propertyName = "spaceDevelopers";
+
+               List<CfContainerValueChanged> addedSpaceDevelopers = spaceDevelopers.stream()
+                       .peek(s ->  log.debug("Appending", propertyName,"container change with added entry:", s))
+                       .map(s -> new CfContainerValueChanged(s, ChangeType.ADDED))
+                       .collect(Collectors.toList());
+               List<String> path = this.extractPathFrom(change);
+               path.add(propertyName);
+               CfContainerChange spaceDevelopersChange = new CfContainerChange(change.getAffectedObject().get(),
+                       propertyName,
+                       path,
+                       addedSpaceDevelopers);
+               cfChanges.add(spaceDevelopersChange);
+            }
+        }
+
         log.verbose("Parsing change type", change.getClass().getSimpleName(), "to custom change type",
                 CfNewObject.class.getSimpleName(), "with object", change.getAffectedObject().get());
-        List<CfChange> cfChanges = Collections.singletonList(new CfNewObject(change.getAffectedObject().get(),
+        cfChanges.add(new CfNewObject(change.getAffectedObject().get(),
                 "",
                 extractPathFrom(change)));
         log.debug("Parsing change type", change.getClass().getSimpleName(), "to custom change type",
--- cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/diff/parsing/NewObjectParsingStrategyTest.java
+++ cloud.foundry.cli/src/test/java/cloud/foundry/cli/logic/diff/parsing/NewObjectParsingStrategyTest.java
@@ -1,12 +1,17 @@
 package cloud.foundry.cli.logic.diff.parsing;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import cloud.foundry.cli.crosscutting.mapping.beans.ApplicationBean;
+import cloud.foundry.cli.crosscutting.mapping.beans.SpecBean;
 import cloud.foundry.cli.logic.diff.change.CfChange;
+import cloud.foundry.cli.logic.diff.change.ChangeType;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerChange;
+import cloud.foundry.cli.logic.diff.change.container.CfContainerValueChanged;
 import cloud.foundry.cli.logic.diff.change.object.CfNewObject;
 import cloud.foundry.cli.logic.diff.change.parsing.NewObjectParsingStrategy;
 import org.hamcrest.Matchers;
@@ -17,6 +22,7 @@ import org.javers.core.diff.changetype.ObjectRemoved;
 import org.javers.core.metamodel.object.GlobalId;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,8 +43,67 @@ public class NewObjectParsingStrategyTest {
         assertThat(changes.size(), Matchers.is(1));
         assertThat(changes.get(0), Matchers.instanceOf(CfNewObject.class));
         assertThat(changes.get(0).getPath(), Matchers.contains("root", "application"));
-        assertThat(changes.get(0).getPropertyName(), Matchers.is(""));
-        assertThat(changes.get(0).getAffectedObject(), Matchers.is(applicationBean));
+        assertThat(changes.get(0).getPropertyName(), is(""));
+        assertThat(changes.get(0).getAffectedObject(), is(applicationBean));
+    }
+
+    @Test
+    public void testParseWithNewSpecBeanObjectAndGivenSpaceDevelopersSucceeds() {
+        GlobalId globalId = mock(GlobalId.class);
+        when(globalId.toString())
+                .thenReturn("#root/spec");
+        List<String> spaceDevelopers = new LinkedList<>();
+        spaceDevelopers.add("spaceDev1");
+        spaceDevelopers.add("spaceDev2");
+        SpecBean specBean = new SpecBean();
+        specBean.setSpaceDevelopers(spaceDevelopers);
+
+        Change change = new NewObject(globalId, Optional.of(specBean), Optional.of(mock(CommitMetadata.class)));
+        NewObjectParsingStrategy strategy = new NewObjectParsingStrategy();
+
+        List<CfChange> changes = strategy.parse(change);
+
+        assertThat(changes, Matchers.notNullValue());
+        assertThat(changes.size(), Matchers.is(2));
+
+        assertThat(changes.get(1), Matchers.instanceOf(CfNewObject.class));
+        assertThat(changes.get(1).getPath(), Matchers.contains("root", "spec"));
+        assertThat(changes.get(1).getPropertyName(), is(""));
+        assertThat(changes.get(1).getAffectedObject(), is(specBean));
+
+        assertThat(changes.get(0), Matchers.instanceOf(CfContainerChange.class));
+        assertThat(changes.get(0).getPath(), Matchers.contains("root", "spec", "spaceDevelopers"));
+        assertThat(changes.get(0).getPropertyName(), is("spaceDevelopers"));
+        assertThat(changes.get(0).getAffectedObject(), is(specBean));
+        CfContainerChange cfContainerChange = (CfContainerChange) changes.get(0);
+        assertThat(cfContainerChange.getChangedValues().size(), is(2));
+        assertThat(cfContainerChange.getChangedValues().get(0), instanceOf(CfContainerValueChanged.class));
+        assertThat(cfContainerChange.getChangedValues().get(0).getChangeType(), is(ChangeType.ADDED));
+        assertThat(cfContainerChange.getChangedValues().get(0).getValue(), is("spaceDev1"));
+        assertThat(cfContainerChange.getChangedValues().get(1), instanceOf(CfContainerValueChanged.class));
+        assertThat(cfContainerChange.getChangedValues().get(1).getChangeType(), is(ChangeType.ADDED));
+        assertThat(cfContainerChange.getChangedValues().get(1).getValue(), is("spaceDev2"));
+    }
+
+    @Test
+    public void testParseWithNewSpecBeanObjectAndNullSpaceDevelopersSucceeds() {
+        GlobalId globalId = mock(GlobalId.class);
+        when(globalId.toString())
+                .thenReturn("#root/spec");
+        SpecBean specBean = new SpecBean();
+
+        Change change = new NewObject(globalId, Optional.of(specBean), Optional.of(mock(CommitMetadata.class)));
+        NewObjectParsingStrategy strategy = new NewObjectParsingStrategy();
+
+        List<CfChange> changes = strategy.parse(change);
+
+        assertThat(changes, Matchers.notNullValue());
+        assertThat(changes.size(), Matchers.is(1));
+
+        assertThat(changes.get(0), Matchers.instanceOf(CfNewObject.class));
+        assertThat(changes.get(0).getPath(), Matchers.contains("root", "spec"));
+        assertThat(changes.get(0).getPropertyName(), is(""));
+        assertThat(changes.get(0).getAffectedObject(), is(specBean));
     }
 
     @Test
