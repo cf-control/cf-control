fe90cc132423ebb9862dfcd88218baacfd789c0d
TobiCode <40341912+TobiCode@users.noreply.github.com>
2020-07-21 12:48:14 +0200

    Update cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/ResourceProvider.java
    Co-authored-by: yenle2501 <30981043+yenle2501@users.noreply.github.com>

--- cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/ResourceProvider.java
+++ cloud.foundry.cli/src/main/java/cloud/foundry/cli/crosscutting/mapping/ResourceProvider.java
@@ -9,7 +9,7 @@ public class ResourceProvider {
 
     /**
      * Get an Input stream from a resource File. Needs to be closed after usage.
-     * @param filename the Filename of the resource file
+     * @param filename the name of the resource file
      * @param forClass the class, which needs the InputStream
      * @return an InputStream to the resource file
      */
